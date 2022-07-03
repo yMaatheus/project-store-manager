aa3138d734b6c71b5dd81ef686cdcacecb2916b2
yMaatheus <stvgamer74@gmail.com>
2022-07-03 19:25:32 -0300

    refactor: alterando nome das funções

--- models/productsModel.js
+++ models/productsModel.js
@@ -1,15 +1,15 @@
 const connection = require('../database/connection');
 
-const getAllProducts = async () => {
+const getAll = async () => {
   const query = 'SELECT * FROM StoreManager.products';
   const [products] = await connection.execute(query);
   return products;
 };
 
-const getProductById = async (productId) => {
+const getById = async (productId) => {
   const query = 'SELECT * FROM StoreManager.products WHERE id = ?';
   const [product] = await connection.execute(query, [productId]);
   return product[0];
 };
 
-module.exports = { getAllProducts, getProductById };
+module.exports = { getAll, getById };
