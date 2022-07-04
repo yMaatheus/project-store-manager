88291ba56c2c76ce1431a8696bfd391986695a91
yMaatheus <stvgamer74@gmail.com>
2022-07-04 16:06:58 -0300

    test: testando o model de produtos

---
+++ tests/unit/models/productsModel.test.js
@@ -0,0 +1,47 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+
+const connection = require('../../../database/connection');
+const { productsModel } = require('../../../models');
+
+describe('Testando Products Model:', () => {
+  describe('A função getAll', () => {
+
+    before(async () => {
+      const RESULT = [{ id: 1, name: "Martelo de Thor" }, { id: 2, name: "Traje de encolhimento" }];
+      sinon.stub(connection, 'execute').resolves([RESULT]);
+    });
+
+    after(async () => {
+      connection.execute.restore();
+    });
+
+    it('Retorna uma lista', async () => {
+      const products = await productsModel.getAll();
+      expect(products).to.be.an('array');
+    });
+
+    it('Retorna uma lista de produtos', async () => {
+      const product = await productsModel.getAll();
+      expect(product).to.deep.equal([{ id: 1, name: "Martelo de Thor" }, { id: 2, name: "Traje de encolhimento" }]);
+    });
+
+  });
+
+  describe('A função getById', () => {
+
+    before(async () => {
+      const RESULT = { id: 1, name: "Martelo de Thor" };
+      sinon.stub(connection, 'execute').resolves([RESULT]);
+    });
+
+    after(async () => {
+      connection.execute.restore();
+    });
+
+    it('Retorna um produto específico', async () => {
+      const product = await productsModel.getById(1);
+      expect(product).to.deep.equal({ id: 1, name: "Martelo de Thor" });
+    });
+  });
+});
