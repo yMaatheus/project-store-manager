41985ecbbe9ac211e3565bf07ecefae9d502105f
yMaatheus <stvgamer74@gmail.com>
2022-07-04 00:57:26 -0300

    feat: criando rotas e aplicando middleware de erro

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const rescue = require('express-rescue');
 
 const app = express();
+
+app.use(express.json());
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+const routes = require('./routers');
+const middlewares = require('./middlewares');
+
+app.use('/products', rescue(routes.productsRouter));
+app.use(middlewares.errorMiddleware);
 
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
---
+++ routers/productsRouter.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const { productsController } = require('../controllers');
+
+const router = express.Router();
+
+router.get('/', productsController.getAll);
+
+router.get('/:id', productsController.getById);
+
+module.exports = router;
