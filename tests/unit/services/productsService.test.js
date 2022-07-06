0d40baa09b0cb7143ebbf89141d8d48755dc65b8
yMaatheus <stvgamer74@gmail.com>
2022-07-06 04:45:57 -0300

    test: refatorando o codigo e adicionando um novo teste para caso o id do produto não seja valido

--- tests/unit/services/productsService.test.js
+++ tests/unit/services/productsService.test.js
@@ -4,43 +4,41 @@ const sinon = require('sinon');
 const { productsService } = require('../../../services');
 const { productsModel } = require('../../../models');
 
-describe('services/productsService', () => {
+const data = [{ id: 1, name: "Martelo de Thor" }, { id: 2, name: "Traje de encolhimento" }];
 
+describe('services/productsService', () => {
+  afterEach(sinon.restore);
   describe('A função getAll', () => {
 
-    before(() => {
-      const RESULT = [{ id: 1, name: "Martelo de Thor" }, { id: 2, name: "Traje de encolhimento" }];
+    it('Retorna uma lista', async () => {
+      sinon.stub(productsModel, 'getAll').resolves(data);
 
-      sinon.stub(productsModel, 'getAll').resolves(RESULT);
-    });
-
-    after(() => {
-      productsModel.getAll.restore();
-    });
-
-    it('Retorna um array', async () => {
       const products = await productsService.getAll();
 
       expect(products).to.be.an('array');
     });
 
-    it('Retorna produtos', async () => {
+    it('Retorna uma lista de produtos', async () => {
+      sinon.stub(productsModel, 'getAll').resolves(data);
+
       const products = await productsService.getAll();
 
-      expect(products).to.deep.equal([{ id: 1, name: "Martelo de Thor" }, { id: 2, name: "Traje de encolhimento" }]);
+      expect(products).to.deep.equal(data);
+    });
+
+    it('Se a lista de produtos estiver vazia retorna null', async () => {
+      sinon.stub(productsModel, 'getAll').resolves([]);
+
+      const products = await productsService.getAll();
+
+      expect(products).to.equal(null);
     });
   });
 
   describe('A função getById', () => {
 
-    before(() => {
-      const RESULT = [{ id: 1, name: "Martelo de Thor" }];
-
-      sinon.stub(productsModel, 'getById').resolves(RESULT);
-    });
-
-    after(() => {
-      productsModel.getById.restore();
+    beforeEach(() => {
+      sinon.stub(productsModel, 'getById').resolves([data[0]]);
     });
 
     it('Retorna um objeto', async () => {
@@ -49,14 +47,13 @@ describe('services/productsService', () => {
       expect(product).to.be.an('object');
     });
 
-    it('Retona um produto com id pedido', async () => {
-      const id = 1;
-      const product = await productsService.getById(id);
+    it('Retona o id pedido', async () => {
+      const product = await productsService.getById(1);
 
-      expect(product.id).to.equal(id);
+      expect(product.id).to.equal(1);
     });
 
-    it('Retorna null se o id não for válido', async () => {
+    it('Se o id não for válido retorna null ', async () => {
       const product = await productsService.getById();
 
       expect(product).to.be.null;
