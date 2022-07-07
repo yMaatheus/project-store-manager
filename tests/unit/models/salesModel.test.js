e21f7fdca3f27c364e066b27d76277760cee4dae
yMaatheus <stvgamer74@gmail.com>
2022-07-07 04:49:36 -0300

    test: testando as funções getAll e getById da camada models

--- tests/unit/models/salesModel.test.js
+++ tests/unit/models/salesModel.test.js
@@ -29,4 +29,51 @@ describe('models/salesModel', () => {
       expect(product).to.deep.equal({ productId: 2, quantity: 3 });
     });
   });
+
+  describe('getAll', () => {
+    it('Retorna uma lista de vendas', async () => {
+      const data = [
+        {
+          saleId: 1,
+          date: "2022-07-07T07:38:08.000Z",
+          productId: 1,
+          quantity: 5
+        },
+        {
+          saleId: 2,
+          date: "2022-07-07T07:38:08.000Z",
+          productId: 3,
+          quantity: 15
+        }
+      ]
+      sinon.stub(connection, 'execute').resolves([data]);
+
+      const sales = await salesModel.getAll();
+
+      expect(sales).to.deep.equal(data);
+    });
+  })
+
+  describe('getById', () => {
+    it('Retorna a lista de vendas com o id pedido', async () => {
+      const data = [
+        {
+          date: "2022-07-07T07:38:08.000Z",
+          productId: 1,
+          quantity: 5
+        },
+        {
+          date: "2022-07-07T07:38:08.000Z",
+          productId: 2,
+          quantity: 10
+        }
+      ]
+
+      sinon.stub(connection, 'execute').resolves([data]);
+
+      const sales = await salesModel.getById(1);
+
+      expect(sales).to.deep.equal(data);
+    });
+  })
 });
