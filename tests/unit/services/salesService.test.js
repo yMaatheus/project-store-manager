36d067ae2961cd21ccb24fc5c7c0065e23e6ccaf
yMaatheus <stvgamer74@gmail.com>
2022-07-07 04:57:25 -0300

    test: testando as funções getAll e getById da camada services

--- tests/unit/services/salesService.test.js
+++ tests/unit/services/salesService.test.js
@@ -2,7 +2,7 @@ const { expect } = require('chai');
 const sinon = require('sinon');
 
 const { salesService } = require('../../../services');
-const { salesModel, productsModel } = require('../../../models');
+const { salesModel } = require('../../../models');
 
 describe('services/salesService', () => {
   afterEach(sinon.restore);
@@ -38,4 +38,76 @@ describe('services/salesService', () => {
       expect(sale).to.deep.equal({ id: 1, itemsSold: [{ productId: 1, quantity: 1 }, { productId: 2, quantity: 5 }] });
     });
   });
+
+  describe('getAll', () => {
+    it('Se o sale for invalido retorna null', async () => {
+      sinon.stub(salesModel, 'getAll').resolves([]);
+
+      const sales = await salesService.getAll();
+
+      expect(sales).to.be.null;
+    });
+
+    it('Retorna a lista de vendas', async () => {
+      const data = [
+        {
+          saleId: 1,
+          date: "2022-07-07T07:38:08.000Z",
+          productId: 1,
+          quantity: 5
+        },
+        {
+          saleId: 2,
+          date: "2022-07-07T07:38:08.000Z",
+          productId: 3,
+          quantity: 15
+        }
+      ]
+
+      sinon.stub(salesModel, 'getAll').resolves(data);
+
+      const sales = await salesService.getAll();
+
+      expect(sales).to.deep.equal(data);
+    });
+  })
+
+  describe('getById', () => {
+    it('Se o id for invalido retorna null', async () => {
+      const sales = await salesService.getById();
+
+      expect(sales).to.be.null;
+    });
+
+    it('Se o sale for invalido retorna null', async () => {
+      sinon.stub(salesModel, 'getById').resolves([]);
+
+      const sales = await salesService.getById(1);
+
+      expect(sales).to.be.null;
+    });
+
+    it('Retorna a lista de vendas', async () => {
+      const data = [
+        {
+          saleId: 1,
+          date: "2022-07-07T07:38:08.000Z",
+          productId: 1,
+          quantity: 5
+        },
+        {
+          saleId: 2,
+          date: "2022-07-07T07:38:08.000Z",
+          productId: 3,
+          quantity: 15
+        }
+      ]
+
+      sinon.stub(salesModel, 'getById').resolves(data);
+
+      const sales = await salesService.getById(1);
+
+      expect(sales).to.deep.equal(data);
+    });
+  })
 });
