4ced953eb5adf5cd7aa06b4dc711daac516da8cd
Matheus <stvgamer74@gmail.com>
2022-11-13 18:39:41 -0300

    fix: :bug: add async in error middleware function

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -1,1 +1,5 @@
-module.exports = (err, _req, res, _next) => res.status(500).json({ message: err.message });
+module.exports = async (err, _req, res, _next) => 
+  // const { message } = err;
+
+  // return res.status().json({ message });
+  res.status(err.code || 500).json({ message: err.message || 'Internal server error' });
